9fc11a26d4723b7e031d2fe689571e2cb7ed6d28
Rubait Reshad <rubaitreshad@gmail.com>
2026-08-03 11:26:27 +0600

    fix: remove unnecessary flex container from body, fixing shrink-wrapped page width

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,7 +43,7 @@ export default async function RootLayout({
       lang="en"
       className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", notoSans.variable, playfairDisplayHeading.variable)}
     >
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-full">
         <Nav session={session} students={students} />
         {children}
       </body>
